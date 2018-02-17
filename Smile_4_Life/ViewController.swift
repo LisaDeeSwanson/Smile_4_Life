d56fd52f07dc469d89fa2006fc201423b373a676
lisaswanson <lds1233@gmail.com>
2018-02-17 11:07:53 -0800

    Initial Commit

---
+++ Smile_4_Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smile_4_Life
+//
+//  Created by Lisa Swanson on 2/17/18.
+//  Copyright © 2018 Technology Speaks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
